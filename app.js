49fa0b0371bc73ea29b2c37e8548c7eb670a4fbe
HQhanqiZHQ <59696760+HQhanqiZHQ@users.noreply.github.com>
2022-12-13 21:02:41 -0500

    add second vis

--- app.js
+++ app.js
@@ -6,12 +6,18 @@ let viz;
 const url = "https://public.tableau.com/views/Superstore_24/Overview";
 
 const vizContainer = document.getElementById('vizContainer');
+
+//sub1
+let viz1;
+const url1 = "https://public.tableau.com/views/liveintegration1/Dashboard1";
+const vizContainerSub1 = document.getElementById('vizContainerSub1');
+
 const options = {
     hideTabs: true,
     height: 1000,
     width: 1200,
     onFirstInteraction: function() {
-        workbook = viz.getWorkbook();
+        workbook = viz1.getWorkbook();
         activeSheet = workbook.getActiveSheet();
         console.log("My dashboard is interactive");
     }
@@ -21,6 +27,12 @@ const options = {
 function initViz() {
     console.log('Executing the initViz function!');
     viz = new tableau.Viz(vizContainer, url, options);
+}
+
+//create a function to generate the viz element
+function initViz1() {
+    console.log('Executing the initViz function!');
+    viz1 = new tableau.Viz(vizContainerSub1, url1, options1);
 }
 
 // run the initViz function when the page loads
