d7fb65f4ec1eeeef2f2b4fe5699b2da6610a656b
HQhanqiZHQ <59696760+HQhanqiZHQ@users.noreply.github.com>
2022-12-13 21:42:01 -0500

    two vis complete debugging

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 console.log('Is this working?');
 
 let viz;
-
 //add Share Link to Tableau Public in here
 const url = "https://public.tableau.com/views/Superstore_24/Overview";
-
 const vizContainer = document.getElementById('vizContainer');
 
 //sub1
@@ -12,7 +10,27 @@ let viz1;
 const url1 = "https://public.tableau.com/views/liveintegration1/Dashboard1";
 const vizContainerSub1 = document.getElementById('vizContainerSub1');
 
+//sub2
+let viz2;
+const url2 = "https://public.tableau.com/views/LearnEmbeddedAnalytics/SalesOverviewDashboard";
+const vizContainerSub2 = document.getElementById('vizContainerSub2');
+
+//sub3
+let viz3;
+const url3 = "https://public.tableau.com/views/SuperbowlOutcomes/Final?%3Adisplay_static_image=y&amp";
+const vizContainerSub3 = document.getElementById('vizContainerSub3');
+
 const options = {
+    hideTabs: true,
+    height: 1000,
+    width: 1200,
+    onFirstInteraction: function() {
+        workbook = viz.getWorkbook();
+        activeSheet = workbook.getActiveSheet();
+        console.log("My dashboard is interactive");
+    }
+};
+const options1 = {
     hideTabs: true,
     height: 1000,
     width: 1200,
@@ -23,17 +41,39 @@ const options = {
     }
 };
 
+const options2 = {
+    hideTabs: true,
+    height: 1000,
+    width: 1200,
+    onFirstInteraction: function() {
+        workbook = viz2.getWorkbook();
+        activeSheet = workbook.getActiveSheet();
+        console.log("My dashboard is interactive");
+    }
+};
+
+
+const options3 = {
+    hideTabs: true,
+    height: 1000,
+    width: 1200,
+    onFirstInteraction: function() {
+        workbook = viz3.getWorkbook();
+        activeSheet = workbook.getActiveSheet();
+        console.log("My dashboard is interactive");
+    }
+};
+
+
 //create a function to generate the viz element
 function initViz() {
     console.log('Executing the initViz function!');
     viz = new tableau.Viz(vizContainer, url, options);
+    viz1 = new tableau.Viz(vizContainerSub1, url1, options1);
+    viz2 = new tableau.Viz(vizContainerSub2, url2, options2);
+    viz3 = new tableau.Viz(vizContainerSub3, url3, options3);
 }
 
-//create a function to generate the viz element
-function initViz1() {
-    console.log('Executing the initViz function!');
-    viz1 = new tableau.Viz(vizContainerSub1, url1, options1);
-}
 
 // run the initViz function when the page loads
 document.addEventListener("DOMContentLoaded", initViz);
